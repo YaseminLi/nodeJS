1ef5adc422d764e0c0cd370064566a86768240ac
YaseminLi <yaseminli@163.com>
2019-12-02 18:48:23 +0800

    删 

--- building/babel.config.js
+++
@@ -1,8 +0,0 @@
-const presets = [
-    [
-      "@babel/env",{ useBuiltIns: "usage"}
-    ],
-    [ "@babel/preset-react"]
-  ];
-  
-  // module.exports = { presets };
--- building/src/script/util.js
+++
@@ -1,9 +0,0 @@
-Object.assign({a:1},{b:2});
-
-export function fn1(){
-    console.log('this is fn1'); 
-};
-
-export function fn2(){
-    console.log('this is fn2'); 
-};
